091726396be850f03ab5342b2f211ba22c712325
luob <luob1215@gmail.com>
2023-12-05 17:18:22 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/luob1215/pijs_doc/',
+  baseUrl: '/pijs_doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
